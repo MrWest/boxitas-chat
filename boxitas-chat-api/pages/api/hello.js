12cc735e8b37157acd56e0dad268e1a87c47950f
MrWest <kidwest87@gmail.com>
2021-02-01 13:41:11 -0500

    3

--- boxitas-chat-api/pages/api/hello.js
+++ boxitas-chat-api/pages/api/hello.js
@@ -1,12 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
 
 export default (req, res) => {
 

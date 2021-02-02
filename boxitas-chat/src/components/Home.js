fbce2f1dcbb05161c0a9f63a26ce468e75ce9228
MrWest <kidwest87@gmail.com>
2021-02-02 13:41:02 -0500

    fix: home chat button to chat view

--- boxitas-chat/src/components/Home.js
+++ boxitas-chat/src/components/Home.js
@@ -1,3 +1,5 @@
+import { Button } from "@material-ui/core";
+import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import CenteredFrame from "./globals/CenterdFrame";
 
@@ -5,10 +7,20 @@ const Title = styled.h1`
  margin: 92px 16px;
  text-align: center;
 `;
-const Home = () => (
+const ButtonContainer = styled.div`
+    padding: 56px;
+`;
+
+const Home = () => {
+    const location = useLocation();
+    return (
     <CenteredFrame>
         <Title>You're Home</Title>
+        <ButtonContainer>
+             <Button size="large" variant="contained" onClick={() => location.push('/chat')}>Let's Chat!!!</Button>
+        </ButtonContainer>
     </CenteredFrame>
-);
+    );
+};
 
 export default Home;

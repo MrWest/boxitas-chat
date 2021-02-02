ce19c5eddd0ed8236e513c682dc2dc4e738ce01f
MrWest <kidwest87@gmail.com>
2021-02-02 01:37:40 -0500

    fix: jsonServer get with payload approach

--- boxitas-chat/src/apis/jsonServer.js
+++ boxitas-chat/src/apis/jsonServer.js
@@ -16,8 +16,8 @@ const jsonServer = () => {
   const del = (path, payload) => {
     return axios.delete(`${baseUrl}/${path}`, payload);
   }
-  const get = path  => {
-        return axios.get(`${baseUrl}/${path}`);
+  const get = (path, payload)  => {
+        return axios.get(`${baseUrl}/${path}${payload ? `/${payload}` : ''}`);
   }
   const filter = (path, query, sort = { order: 'desc' })  => {
     return axios.get(`${baseUrl}/${path}?${query}${sort.sort? `&_sort=${sort.sort}&_order=${sort.order || 'desc'}`: ''}`);
